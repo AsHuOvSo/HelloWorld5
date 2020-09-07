fdb6de54ca28aeeb97064ecadb035211095e9edb
AsHuOvSo <asta@avra.dk>
2020-09-07 11:41:16 +0200

    Initial commit

---
+++ src/Hello.java
@@ -0,0 +1,10 @@
+public class Hello {
+    //psvm
+    public static void main(String[] args) {
+        //sout
+        System.out.println("Hello World!");
+        System.out.println("This is my first Java program.");
+        System.out.println("I have coded in C# before.");
+        System.out.println("But this is different.");
+    }
+}
